a875d3c432840658fe68d169c4900e2d96324a38
ELITEprogram001 <ridgeway.jared@gmail.com>
2022-04-14 14:37:53 -0500

    added darvo deal tile details

--- src/components/DarvoDeals.js
+++ src/components/DarvoDeals.js
@@ -1,9 +1,51 @@
+import axios from 'axios'
+import {useState, useEffect} from 'react'
 import '../styles/DarvoDeals.css'
 
 export default function DarvoDeals({time, deal}) {
+
+    const [imgSrc, setImgSrc] = useState('')
+
+    async function getItem() {
+        const res = await axios.get(`/api/v1/items/${deal.item}`)
+        setImgSrc(res.data.data.imageName)
+    }
+
+    function isSoldOut() {
+        return deal.total - deal.sold <= 0
+    }
+
+    function percentItemsLeft() {
+        return ((deal.total - deal.sold) / deal.total) * 100
+    }
+
+    useEffect(() => {
+        getItem()
+    }, [deal])
+
     return (
-        <div>
-            <h1 className='deal-item-name status-title'>{deal.item}</h1>
+        <div className='deal-tile'>
+            {isSoldOut() && 
+                <div className='out-of-stock-overlay'>
+                    <div className='out-of-stock-banner '>OUT OF STOCK</div>
+                </div>
+            }
+            <div className='deal-wrapper'>
+                <h1 className='deal-item-name status-title'>
+                    {deal.item}
+                    <span>({`${deal.discount}% off` })</span>
+                </h1>
+                <p>Sale Price: {`${deal.salePrice} platinum`}</p>
+                <p>Original Price: {`${deal.originalPrice} platinum`}</p>
+                {imgSrc && <img src={`https://cdn.warframestat.us/img/${imgSrc}`} alt='daily deal item'/>}
+                <div className='stock-info'>
+                    <p>Stock: {`${deal.total - deal.sold} / ${deal.total} left`}</p>
+                    <div className='items-out meter'>
+                        <div className='items-left meter' style={{width: `${percentItemsLeft()}%`}}>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/WorldStatusTile.js
+++ src/components/WorldStatusTile.js
@@ -5,11 +5,9 @@ export default function WorldStatusTile({time, title, world}) {
 
     return (
         <div className='world-status'>
-            <div className='status-wrapper'>
-                <h2 className='status-title'>{title}</h2>
-                <p>State: {world.state || world.active}</p>
-                <p>Time Remaining: {time}</p>
-            </div>
+            <h2 className='status-title'>{title}</h2>
+            <p>State: {world.state || world.active}</p>
+            <p>Time Remaining: {time}</p>
         </div>
     )
 }
