57df988fb8fdaabdacd96d55c10eeb94d3185f42
ELITEprogram001 <ridgeway.jared@gmail.com>
2022-04-20 13:23:52 -0500

    updated worldstatus container

--- src/components/WorldStatusTile.js
+++ src/components/WorldStatusTile.js
@@ -1,13 +1,27 @@
 import {useState, useEffect} from 'react'
-import '../styles/WorldStatusQuickView.css'
+import styles from '../styles/WorldStatusTile.module.css'
 
 export default function WorldStatusTile({time, title, world}) {
 
+    const {state, active} = world
+    let colorClass = ''
+    if(state === 'cold' || active === 'voss') colorClass = 'coldAndVoss'
+    else if(state === 'warm' || active === 'fass') colorClass = 'warmAndFass'
+    else if(state === 'day') colorClass = 'yellow'
+    else if(state === 'night') colorClass = 'purple'
+
     return (
-        <div className='world-status'>
-            <h2 className='status-title'>{title}</h2>
-            <p>State: {world.state || world.active}</p>
-            <p>Time Remaining: {time}</p>
+        <div className={styles.status}>
+            <div className={styles.titleAndTimerWrapper}>
+                <h2 className={styles.title}>{title}</h2>
+                <span className={styles.worldState}>
+                    (<span className={styles[colorClass]}>
+                        {world.state || world.active}
+                    </span>)
+                </span>
+                <span className={styles.timer}>{time}</span>
+            </div>
+            <p className={styles.eta}></p>
         </div>
     )
 }
